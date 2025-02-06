e91f537395f852d0a422ac5f3c015c1130488fb1
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-02-06 15:57:23 +0300

    11th problem

---
+++ Sereja-and-Dima.py
@@ -0,0 +1,23 @@
+n = int(input())
+cards = list(map(int, input().split()))
+
+sereja, dima = 0, 0
+turn = True
+
+left, right = 0, n - 1
+while left <= right:
+    if cards[left] > cards[right]:
+        chosen = cards[left]
+        left += 1
+    else:
+        chosen = cards[right]
+        right -= 1
+
+    if turn:
+        sereja += chosen
+    else:
+        dima += chosen
+
+    turn = not turn
+
+print(sereja, dima)
